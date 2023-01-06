8d0d86221cdde98f4c7418ccb637cf49d4ddc187
Takuma Homma <nagomimatcha@gmail.com>
2023-01-06 21:23:54 +0900

    Show current position

--- app/src/main/java/com/mataku/jetpackcomposesandbox/ui/compose/component/LoopingHorizontalPager.kt
+++ app/src/main/java/com/mataku/jetpackcomposesandbox/ui/compose/component/LoopingHorizontalPager.kt
@@ -3,9 +3,18 @@
 package com.mataku.jetpackcomposesandbox.ui.compose.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -13,8 +22,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.accompanist.pager.ExperimentalPagerApi
@@ -35,44 +48,111 @@ fun LoopingHorizontalBannerPagerSample(
   } else {
     bannerList
   }
-
   val pagerState = rememberPagerState(initialPage = scrollableList.size / 2)
-  HorizontalPager(
-    state = pagerState,
-    count = scrollableList.size,
-    modifier = modifier,
-    content = { index ->
-      val banner = scrollableList[index]
-      Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-      ) {
-        Image(
-          painter = painterResource(id = banner.resId),
-          contentDescription = "banner image",
-          modifier = Modifier
-            .fillMaxWidth(),
-          contentScale = ContentScale.Crop,
-        )
-        Text(
-          text = banner.title,
-          fontSize = 16.sp,
-          modifier = Modifier
-            .padding(16.dp),
-        )
+  Column(modifier = Modifier.fillMaxSize()) {
+    HorizontalPager(
+      state = pagerState,
+      count = scrollableList.size,
+      modifier = modifier,
+      content = { index ->
+        val banner = scrollableList[index]
+        Column(
+          modifier = Modifier.fillMaxWidth(),
+          horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+          Image(
+            painter = painterResource(id = banner.resId),
+            contentDescription = "banner image",
+            modifier = Modifier
+              .fillMaxWidth(),
+            contentScale = ContentScale.Crop,
+          )
+          Text(
+            text = banner.title,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+              .padding(16.dp),
+          )
+        }
+      }
+    )
+    if (pagerState.currentPage == 0 && !pagerState.isScrollInProgress) {
+      LaunchedEffect(Unit) {
+        pagerState.scrollToPage(scrollableList.lastIndex - 1)
       }
     }
-  )
-  if (pagerState.currentPage == 0 && !pagerState.isScrollInProgress) {
-    LaunchedEffect(Unit) {
-      pagerState.scrollToPage(scrollableList.lastIndex - 1)
+
+    if (pagerState.currentPage == scrollableList.lastIndex && !pagerState.isScrollInProgress) {
+      LaunchedEffect(Unit) {
+        pagerState.scrollToPage(1)
+      }
+    }
+
+    val currentPosition = pagerState.currentPage
+
+    Spacer(modifier = Modifier.height(24.dp))
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+      Text(
+        text = "Current position",
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(8.dp)
+      )
+
+      Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+      ) {
+        IntRange(0, scrollableList.lastIndex).forEach {
+          TextInCircle(
+            text = it.toString(),
+            selected = currentPosition == it,
+            modifier = Modifier
+              .padding(16.dp)
+          )
+        }
+      }
     }
   }
+}
 
-  if (pagerState.currentPage == scrollableList.lastIndex && !pagerState.isScrollInProgress) {
-    LaunchedEffect(Unit) {
-      pagerState.scrollToPage(1)
-    }
+@Composable
+private fun TextInCircle(
+  text: String,
+  modifier: Modifier = Modifier,
+  selected: Boolean
+) {
+  val borderColor = if (selected) {
+    MaterialTheme.colors.onSurface
+  } else {
+    Color.LightGray
+  }
+  val borderWidth = if (selected) {
+    2.dp
+  } else {
+    1.dp
+  }
+  Box(
+    modifier = modifier
+      .size(32.dp)
+      .clip(CircleShape)
+      .border(
+        width = borderWidth,
+        color = borderColor,
+        shape = CircleShape
+      ),
+    contentAlignment = Alignment.Center
+  ) {
+    Text(
+      text = text,
+      fontSize = 16.sp,
+      color = Color.Gray,
+      fontWeight = FontWeight.Bold
+    )
   }
 }
 
@@ -86,9 +166,50 @@ private fun LoopingHorizontalBannerPagerPreview() {
           Banner(
             resId = R.drawable.ic_launcher_background,
             title = "title A"
+          ),
+          Banner(
+            resId = R.drawable.ic_launcher_background,
+            title = "title A"
+          ),
+          Banner(
+            resId = R.drawable.ic_launcher_background,
+            title = "title A"
           )
         )
       )
+    }
+  }
+}
+
+@Composable
+@MultiThemePreview
+private fun TextInCirclePreview() {
+  MaterialTheme {
+    Surface {
+      Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+      ) {
+        TextInCircle(
+          text = "1",
+          selected = false,
+          modifier = Modifier
+            .padding(16.dp)
+        )
+        TextInCircle(
+          text = "2",
+          selected = true,
+          modifier = Modifier
+            .padding(16.dp)
+        )
+        TextInCircle(
+          text = "3",
+          selected = false,
+          modifier = Modifier
+            .padding(16.dp)
+        )
+      }
     }
   }
 }
--- app/src/main/java/com/mataku/jetpackcomposesandbox/ui/preview/MultiThemePreview.kt
+++ app/src/main/java/com/mataku/jetpackcomposesandbox/ui/preview/MultiThemePreview.kt
@@ -4,6 +4,6 @@ import android.content.res.Configuration
 import androidx.compose.ui.tooling.preview.Preview
 
 
-@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 annotation class MultiThemePreview
