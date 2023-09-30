2ec020b558ea19d7ddb776447478c56dd7ba004a
Takuma Homma <nagomimatcha@gmail.com>
2023-10-01 00:14:47 +0900

    Update AGP

--- build-logic/convention/src/main/java/ext/TestConfiguration.kt
+++ build-logic/convention/src/main/java/ext/TestConfiguration.kt
@@ -2,16 +2,16 @@ package ext
 
 import com.android.build.api.dsl.CommonExtension
 
-fun CommonExtension<*, *, *, *>.testConfiguration() {
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-            all {
-                it.maxParallelForks = 2
-            }
-        }
+fun CommonExtension<*, *, *, *, *>.testConfiguration() {
+  testOptions {
+    unitTests {
+      isIncludeAndroidResources = true
+      all {
+        it.maxParallelForks = 2
+      }
     }
-    defaultConfig {
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+  }
+  defaultConfig {
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
 }
