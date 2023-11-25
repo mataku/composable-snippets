9d6556d5dcc0d0adae8ea1f0b434e37991d063a8
Takuma Homma <nagomimatcha@gmail.com>
2023-11-26 01:19:21 +0900

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,19 +4,15 @@ buildscript {
     mavenCentral()
     maven("https://plugins.gradle.org/m2/")
   }
-  dependencies {
-    classpath(libs.android.gradle.plugin)
-    classpath(libs.kotlin.gradle.plugin)
-    classpath(libs.hilt.gradle.plugin)
-
-    // NOTE: Do not place your application dependencies here; they belong
-    // in the individual module build.gradle files
-  }
 }
 
 plugins {
-  id("com.google.gms.google-services") version "4.3.15" apply false
-  id("com.google.devtools.ksp") version libs.versions.ksp.get() apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.dagger.hilt) apply false
+  alias(libs.plugins.kotlin) apply false
+  alias(libs.plugins.ksp)
+  alias(libs.plugins.google.services) // ensure to apply at the bottom
 }
 
 allprojects {
