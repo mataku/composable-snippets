ace009dcb8317b4f60b7805ae10d473281254f33
Takuma Homma <nagomimatcha@gmail.com>
2023-12-10 19:57:08 +0900

    Create skeletons

---
+++ app/src/main/java/com/mataku/snippets/ui/compose/sample/InputActionScreen.kt
@@ -0,0 +1,15 @@
+package com.mataku.snippets.ui.compose.sample
+
+import androidx.compose.runtime.Composable
+import com.google.android.catalog.framework.annotations.Sample
+
+@Composable
+@Sample(
+  name = "Input actions Sample",
+  description = "Input actions",
+  tags = ["Swipe", "Move", "Drag"],
+  sourcePath = "https://github.com/mataku/composable-snippets/blob/develop/app/src/main/java/com/mataku/jetpackcomposesandbox/ui/compose/sample/InputActionScreen.kt"
+)
+fun InputActionScreen() {
+
+}
