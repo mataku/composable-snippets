6508ea66fecb66090c23d1c79167fdc0589857c6
Takuma Homma <nagomimatcha@gmail.com>
2025-07-12 13:46:35 +0900

    Fix new API

--- build-logic/convention/src/main/java/ext/ComposeConfiguration.kt
+++ build-logic/convention/src/main/java/ext/ComposeConfiguration.kt
@@ -5,6 +5,8 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 fun Project.composeConfiguration() {
   with(pluginManager) {
@@ -17,6 +19,13 @@ fun Project.composeConfiguration() {
         compose = true
       }
 
+      with(extensions.getByType<KotlinAndroidProjectExtension>()) {
+        compilerOptions {
+          jvmTarget.set(JvmTarget.JVM_17)
+          // painful but too many files target
+          freeCompilerArgs.add("-opt-in=androidx.compose.animation.ExperimentalSharedTransitionApi")
+        }
+      }
       with(extensions.getByType<ComposeCompilerGradlePluginExtension>()) {
         val composeReportEnabled =
           rootProject.providers.gradleProperty("composeCompilerReports").orNull == "true"
